8293cb028aae9e46c5e0218f0ea018f730b73162
rojait <roja.viswa@gmail.com>
2018-11-04 12:01:29 -0800

    Palindrome Program

---
+++ JavaPrograms/src/BasicPrograms/PalindromeNumberString.java
@@ -0,0 +1,36 @@
+package BasicPrograms;
+
+//Palindrome number is a number that is same as reverse 
+//example- 545, 151, 34543 
+
+import java.util.Scanner;
+public class PalindromeNumberString {
+
+	public static void main(String args[]) {
+
+		// 1. create two variables
+		String original, reverse = "";// Objects of string class
+
+		// 2. Get the number to check whether the number is palindrome.
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a string/number to check if it is a palindrome ");
+		original = input.nextLine();
+
+		// 3. create a variable to find the length of the string
+		int length = original.length();
+
+		// 4. create for loop to reverse the number
+		for (int i = length - 1; i >= 0; i--) {
+			reverse = reverse + original.charAt(i);
+		}
+
+		// 5. checking if the number is palindrome or not
+		if (original.equals(reverse))
+			System.out.println("Entered string/number is a palindrome.");
+		else
+			System.out.println("Entered string/number is not palindrome.");
+		input.close();
+
+	}
+}
+
