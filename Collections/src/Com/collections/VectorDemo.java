9fe07f8ed72b13a62f150172d8ca3e38250da006
rojait <roja.viswa@gmail.com>
2018-12-10 08:49:32 -0800

    Vector demo

---
+++ Collections/src/Com/collections/VectorDemo.java
@@ -0,0 +1,20 @@
+package Com.collections;
+
+import java.util.List;
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		List<String> stringlist1 = new Vector<>();
+		stringlist1.add("Rama");
+		stringlist1.add("sita");
+		stringlist1.add("Laxman");
+		stringlist1.add("Ravana");
+		
+	   System.out.println(stringlist1);
+	
+		
+	}
+
+}
