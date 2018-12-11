00aee6d470878b3e184f9d759db5ea0828a5b01c
rojait <roja.viswa@gmail.com>
2018-12-11 12:38:13 -0800

    sum of n mumbers

---
+++ JavaPrograms/src/Programs/SumOfNNumbers.java
@@ -0,0 +1,32 @@
+package Programs;
+
+public class SumOfNNumbers {
+	public static void print_sums(){
+
+        int sum = 0 ;
+         for(int i = 1 ; i <= 6 ; i++){
+             for(int j = 1 ; j <= i; j++ ){
+                sum = sum + j ;
+             }
+             System.out.println(sum) ;
+             sum = 0 ;
+         }
+
+    }
+
+    public static void main(String[] args)
+    {
+        print_sums() ;
+
+
+    }
+}
+/*output
+1
+3
+6
+10
+15
+21
+
+*/
